21331f46925a2e4e345de2e57cc22a40c0214472
asalazar5628-art <asalazar5628@gmail.com>
2026-04-18 16:02:06 -0700

    changed titles array a bit
    I hav echanged the title of the cards to refer back to the player objects im creating. Seems to work just fine. I alos changed one line on the html file

--- scripts.js
+++ scripts.js
@@ -23,17 +23,20 @@
  *
  */
 
-//images for the soccer players.
+//images for the soccer players. For the players I couldnt find the card for I entered an old photo.
 const FRESH_PRINCE_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNA8hvl4uoLJZw1lYkO6oOCXG9fu3d1Vz89w&s";
 const CURB_POSTER_URL =
   "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
 const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoaw-Br5Qi8Nb4YdS2s6cxJ3fX53bbM8TSDw&s";
 
 //player details name, pacing, dribble, shooting, defense, passing, physicality
-function Player(name, PAC, DRI, SHO, DEF, PAS, PHY){
+function Player(name, height, pos, rating, PAC, DRI, SHO, DEF, PAS, PHY){
   this.name = name;
+  this.height = height;
+  this.pos = pos;
+  this.rating = rating;
   this.PAC = PAC;
   this.DRI = DRI;
   this.SHO = SHO;
@@ -42,11 +45,15 @@ function Player(name, PAC, DRI, SHO, DEF, PAS, PHY){
   this.PHY = PHY;
 }
 
+const player1 = new Player("Lionel Messi", "5'7", "CF", 94, 93, 97, 87, 46, 82, 67);
+const player2 = new Player("Xavier Hernández Creus", "5'7", "CM", 92, 93, 97, 87, 46, 82, 67);
+const player3 = new Player("Cristiano Ronaldo", "6'1", "CF", 92, 92, 93, 90, 59, 79, 89);
+
 // This is an array of strings (TV show titles)
 let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+  player1.name,
+  player2.name,
+  player3.name,
 ];
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
