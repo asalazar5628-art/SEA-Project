ee571b37c8affe1a86046e7fb68cf6a087349ff5
AlexanderSalazar189 <asalazar5628@gmail.com>
2026-04-21 17:06:05 -0700

    added the rest of the entries

--- scripts.js
+++ scripts.js
@@ -37,6 +37,36 @@ const INIESTA_URL =
 const ANDREI_URL = 
   "https://www.fifauteam.com/pt/wp-content/uploads/2012/07/A028-1.jpg";
 
+const BENZEMA_URL = 
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmHQhLr2Q2tBRh_NgnpoqwrPxJi4I--8z3lg&s";
+
+const RONALDINHO_URL = 
+  "https://www.futwiz.com/assets/img/fifa12/faces/28130.png";
+
+const PEPE_URL = 
+  "https://cdn.futbin.com/content/fifa12/img/players/120533.png?v=23";
+
+const Pedro_URL = 
+  "https://www.futwiz.com/assets/img/fifa12/faces/189505.png";
+
+const NEYMAR_URL = 
+  "https://i.ytimg.com/vi/WBzvIWiDAz8/oar2.jpg?sqp=-oaymwEYCJUDENAFSFqQAgHyq4qpAwcIARUAAIhC&rs=AOn4CLAL_Y90ioAzU11VJAoLREfgba_EVg&usqp=CCk";
+
+const DANI_URL = 
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZlMixhKi6hXFb10miMKnYxDIN5i6eStj-mw&s"
+
+const DAVID_URL = 
+  "https://www.futwiz.com/assets/img/fifa12/faces/168542.png";
+
+const ROONEY_URL = 
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlhjKISpr70gkr8wocpPjS6cGIsLTT_2R38g&s";
+
+const PIQUE_URL = 
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRExCyVkhKHV0Nna50Qn1klC7fHZHPcy4b9Zw&s";
+
+const ZLATAN_URL = 
+  "https://www.fifauteam.com/pt/wp-content/uploads/2012/07/A028-12.jpg";
+
 //player details name, pacing, dribble, shooting, defense, passing, physicality
 function Player(name, height, pos, rating, PAC, DRI, SHO, DEF, PAS, PHY){
   this.name = name;
@@ -55,8 +85,17 @@ const player1 = new Player("Lionel Messi", "5'7", "CF", 94, 93, 97, 87, 46, 82, 
 const player2 = new Player("Xavier Hernández Creus", "5'7", "CM", 92, 93, 97, 87, 46, 82, 67);
 const player3 = new Player("Cristiano Ronaldo", "6'1", "CF", 92, 92, 93, 90, 59, 79, 89);
 const player4 = new Player("Andrés Iniesta Luján", "5'7", "CAM", 91, 78, 91, 72, 65, 90, 55);
-//const player5 = new Player("Andrei Arshavin", )
-
+const player5 = new Player("Andrei Arshavin", "5'8", "LW", 86, 88, 91, 83, 55, 87, 55);
+const player6 = new Player("Karim Benzema", "6'0", "ST", 84, 87, 85, 80, 52, 65, 72);
+const player7 = new Player("Ronaldinho", "6'0", "CAM", 84, 63, 89, 79, 49, 82, 58);
+const player8 = new Player("Pepe", "6'2", "CB", 85, 75, 60, 48, 84, 62, 83);
+const player9 = new Player("Pedro", "5'7", "LW", 85, 86, 84, 76, 53, 78, 62);
+const player10 = new Player("Neymar Jr", "5'8", "CF", 84, 89, 90, 78, 43, 75, 59);
+const player11 = new Player("Dani Alves", "5'7", "RB", 86, 93, 83, 75, 80, 84, 76);
+const player12 = new Player("David Silva", "5'7", "RW", 89, 82, 91, 84, 55, 90, 61);
+const player13 = new Player("Rooney", "5'9", "ST", 90, 83, 84, 88, 71, 79, 81);
+const player14 = new Player("Pique", "6'4", "CB", 88, 66, 65, 52, 86, 74, 81);
+const player15 = new Player("Zlatan Ibrahimovic", "6'5", "ST", 91, 83, 91, 96, 65, 87, 88);
 
 // This is an array of objects containing what I want attatched to each player
 let players = [
@@ -83,6 +122,72 @@ let players = [
     imageURL: INIESTA_URL,
     //array of what I want shown in that 9 bullet list in the HTML DOC
     bulletPoints: [player4.height, player4.pos, player4.rating, player4.PAC, player4.DRI, player4.SHO, player4.DEF, player4.PAS, player4.PHY]
+  },
+  {
+    playerName: player5.name,
+    imageURL: ANDREI_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player5.height, player5.pos, player5.rating, player5.PAC, player5.DRI, player5.SHO, player5.DEF, player5.PAS, player5.PHY]
+  },
+  {
+    playerName: player6.name,
+    imageURL: BENZEMA_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player6.height, player6.pos, player6.rating, player6.PAC, player6.DRI, player6.SHO, player6.DEF, player6.PAS, player6.PHY]
+  },
+  {
+    playerName: player7.name,
+    imageURL: RONALDINHO_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player7.height, player7.pos, player7.rating, player7.PAC, player7.DRI, player7.SHO, player7.DEF, player7.PAS, player7.PHY]
+  },
+  {
+    playerName: player8.name,
+    imageURL: PEPE_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player8.height, player8.pos, player8.rating, player8.PAC, player8.DRI, player8.SHO, player8.DEF, player8.PAS, player8.PHY]
+  },
+  {
+    playerName: player9.name,
+    imageURL: Pedro_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player9.height, player9.pos, player9.rating, player9.PAC, player9.DRI, player9.SHO, player9.DEF, player9.PAS, player9.PHY]
+  },
+  {
+    playerName: player10.name,
+    imageURL: NEYMAR_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player10.height, player10.pos, player10.rating, player10.PAC, player10.DRI, player10.SHO, player10.DEF, player10.PAS, player10.PHY]
+  },
+  {
+    playerName: player11.name,
+    imageURL: DANI_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player11.height, player11.pos, player11.rating, player11.PAC, player11.DRI, player11.SHO, player11.DEF, player11.PAS, player11.PHY]
+  },
+  {
+    playerName: player12.name,
+    imageURL: DAVID_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player12.height, player12.pos, player12.rating, player12.PAC, player12.DRI, player12.SHO, player12.DEF, player12.PAS, player12.PHY]
+  },
+  {
+    playerName: player13.name,
+    imageURL: ROONEY_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player13.height, player13.pos, player13.rating, player13.PAC, player13.DRI, player13.SHO, player13.DEF, player13.PAS, player13.PHY]
+  },
+  {
+    playerName: player14.name,
+    imageURL: PIQUE_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player14.height, player14.pos, player14.rating, player14.PAC, player14.DRI, player14.SHO, player14.DEF, player14.PAS, player14.PHY]
+  },
+  {
+    playerName: player15.name,
+    imageURL: ZLATAN_URL,
+    //array of what I want shown in that 9 bullet list in the HTML DOC
+    bulletPoints: [player15.height, player15.pos, player15.rating, player15.PAC, player15.DRI, player15.SHO, player15.DEF, player15.PAS, player15.PHY]
   }
 ];
 // Your final submission should have much more data than this, and
